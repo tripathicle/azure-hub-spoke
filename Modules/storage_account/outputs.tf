2de36cae2bbaf91c73344bd613a420d3376e787b
tripathicle <tripathicle@gmail.com>
2026-09-07 18:42:14 +0530

    StorageAccount

---
+++ Modules/storage_account/outputs.tf
@@ -0,0 +1,27 @@
+output "storageaccounts" {
+    description = "All storageaccounts attributes"
+
+    value = {
+
+        # for k , stg in azurerm_storage_account.this :
+
+        # k =>stg.name
+
+        for k , stg in azurerm_storage_account.this :k =>{
+            
+            id = stg.id
+            name = stg.name
+            location = stg.location
+            resource_group_name = stg.resource_group_name
+
+
+        }
+        
+
+
+
+    
+
+    }
+  
+}
